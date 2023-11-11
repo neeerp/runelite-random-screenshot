ba1697cd68a10b4e9fb57bf8c9f97bb21d971d03
David Goldstein <goldstein.g.david@gmail.com>
2023-11-11 18:36:42 -0500

    Add a config flag to enable custom directory, and add warning message when setting flag

--- src/main/java/com/randomscreenshot/RandomScreenshotConfig.java
+++ src/main/java/com/randomscreenshot/RandomScreenshotConfig.java
@@ -26,13 +26,33 @@ public interface RandomScreenshotConfig extends Config
 		return 500;
 	}
 
+	/* TODO: Ideally we'd use a file picker here, but the file picker is not available to the plugin config panel and
+	    making it available requires changes to core runelite. */
 	@ConfigItem(
 		keyName = "screenshotDirectory",
-		name = "Screenshot Directory",
+		name = "Custom Screenshot Directory",
 		description = "Custom directory to save screenshots to",
-		section = customDirectorySection
+		section = customDirectorySection,
+		position = 1
 	)
 	default String screenshotDirectory() {
 		return "";
+	}
+
+	@ConfigItem(
+		keyName = "useCustomDirectory",
+		name = "Use Custom Screenshot Directory",
+		description = "Save screenshots to the configured custom directory",
+		warning =
+			"When enabling this option, make sure that the directory you've configured is valid! " +
+			"If it isn't, screenshots will be lost!\n\n" +
+			"Also note: if you sync your RuneLite settings across multiple computers, " +
+			"please make sure the given directory is valid on all your computers!\n\n" +
+			"You can disregard this message if you are just disabling this option.",
+		section = customDirectorySection,
+		position = 2
+	)
+	default boolean useCustomDirectory() {
+		return false;
 	}
 }
